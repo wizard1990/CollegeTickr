3e2ed82c3cde6f1e640828a1d2d0c7977f7e237d
Max Gu <guxinchi2000@gmail.com>
2014-04-23 01:29:57 -0700

    Get User Info (Issue: Async Block)

--- CollegeTickr/CTAppDelegate.m
+++ CollegeTickr/CTAppDelegate.m
@@ -70,42 +70,79 @@ NSString *const FBSessionStateChangedNotification =
 
 
 #pragma mark - fb
-/*
- * Callback for session changes.
- */
-- (void)sessionStateChanged:(FBSession *)session
-                      state:(FBSessionState) state
-                      error:(NSError *)error
+
+//// Logged-in user experience.  Only triggered with FB Button!
+//- (void)loginViewShowingLoggedInUser:(FBLoginView *)loginView {
+//    NSLog(@"loginViewShowingLoggedInUser has been called, user has logged in.  Place holder for any additional code please put it here");
+//}
+
+// This method will handle ALL the session state changes in the app
+- (void)sessionStateChanged:(FBSession *)session state:(FBSessionState) state error:(NSError *)error
 {
-    switch (state) {
-        case FBSessionStateOpen:
-            if (!error) {
-                // We have a valid session
-                NSLog(@"User session found");
-            }
-            break;
-        case FBSessionStateClosed:
-        case FBSessionStateClosedLoginFailed:
-            [FBSession.activeSession closeAndClearTokenInformation];
-            break;
-        default:
-            break;
+    // If the session was opened successfully
+    if (!error && state == FBSessionStateOpen){
+        NSLog(@"Session opened");
+        // Show the user the logged-in UI
+        [self userLoggedIn];
+        return;
+    }
+    if (state == FBSessionStateClosed || state == FBSessionStateClosedLoginFailed){
+        // If the session is closed
+        NSLog(@"Session closed");
+        // Show the user the logged-out UI
+//        [self userLoggedOut];
     }
     
-    [[NSNotificationCenter defaultCenter]
-     postNotificationName:FBSessionStateChangedNotification
-     object:session];
-    
-    if (error) {
-        UIAlertView *alertView = [[UIAlertView alloc]
-                                  initWithTitle:@"Error"
-                                  message:error.localizedDescription
-                                  delegate:nil
-                                  cancelButtonTitle:@"OK"
-                                  otherButtonTitles:nil];
-        [alertView show];
+    // Handle errors
+    if (error){
+        NSLog(@"Error");
+        NSString *alertText;
+        NSString *alertTitle;
+        // If the error requires people using an app to make an action outside of the app in order to recover
+        if ([FBErrorUtility shouldNotifyUserForError:error] == YES){
+            alertTitle = @"Something went wrong";
+            alertText = [FBErrorUtility userMessageForError:error];
+//            [self showMessage:alertText withTitle:alertTitle];
+        } else {
+            
+            // If the user cancelled login, do nothing
+            if ([FBErrorUtility errorCategoryForError:error] == FBErrorCategoryUserCancelled) {
+                NSLog(@"User cancelled login");
+                
+                // Handle session closures that happen outside of the app
+            } else if ([FBErrorUtility errorCategoryForError:error] == FBErrorCategoryAuthenticationReopenSession){
+                alertTitle = @"Session Error";
+                alertText = @"Your current session is no longer valid. Please log in again.";
+//                [self showMessage:alertText withTitle:alertTitle];
+                
+                // Here we will handle all other errors with a generic error message.
+                // We recommend you check our Handling Errors guide for more information
+                // https://developers.facebook.com/docs/ios/errors/
+            } else {
+                //Get more error information from the error
+                NSDictionary *errorInformation = [[[error.userInfo objectForKey:@"com.facebook.sdk:ParsedJSONResponseKey"] objectForKey:@"body"] objectForKey:@"error"];
+                
+                // Show the user an error message
+                alertTitle = @"Something went wrong";
+                alertText = [NSString stringWithFormat:@"Please retry. \n\n If the problem persists contact us and mention this error code: %@", [errorInformation objectForKey:@"message"]];
+//                [self showMessage:alertText withTitle:alertTitle];
+            }
+        }
+        // Clear this token
+        [FBSession.activeSession closeAndClearTokenInformation];
+        // Show the user the logged-out UI
+//        [self userLoggedOut];
     }
 }
+
+- (void) userLoggedIn {
+    NSLog(@"loginViewShowingLoggedInUser has been called, user has logged in.  Place holder for any additional code please put it here");
+    
+    NSDictionary *userInfo = [self getUserInfo];
+    NSLog(@"Dictionary: %@", [userInfo description]);
+
+}
+
 
 /*
  * Opens a Facebook session and optionally shows the login UX.   //version w/o caching token
@@ -154,8 +191,13 @@ NSString *const FBSessionStateChangedNotification =
         // open call if a cached token was previously found.
         openSessionResult = session.isOpen;
     }
+    
+
+    
     return openSessionResult;
 }
+
+
 - (void) closeSession {
     [FBSession.activeSession closeAndClearTokenInformation];
 }
@@ -171,5 +213,24 @@ NSString *const FBSessionStateChangedNotification =
     return [FBAppCall handleOpenURL:url sourceApplication:sourceApplication];
 }
 
+- (NSDictionary *)getUserInfo {
+    __block NSDictionary *userInfo;
+    [FBRequestConnection startForMeWithCompletionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+        if (!error) {
+            // Success! Include your code to handle the results here
+            //            NSLog(@"user info: %@", result);
+            //            __block NSDictionary *userInfo = [(NSArray *)[result data] objectAtIndex:0];
+            userInfo = result;
+            NSLog(@"Dictionary: %@", [userInfo description]);
+
+        } else {
+            // An error occurred, we need to handle the error
+            // See: https://developers.facebook.com/docs/ios/errors
+        }
+    }];
+    NSLog(@"Dictionary: %@", [userInfo description]);
+
+    return userInfo;
+}
 
 @end
