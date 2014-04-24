639f12e49cf1cfa2c9f5698c88e5c6bde8f6a7d3
wizard1990 <wizard19900509@gmail.com>
2014-04-24 15:35:14 -0700

    conflict done

--- CollegeTickr/CTUserModel.m
+++ CollegeTickr/CTUserModel.m
@@ -15,14 +15,8 @@
 @property (readwrite, nonatomic, strong) NSString *lastName;
 @property (readwrite, nonatomic, strong) NSString *location;
 
-- (instancetype)init
-{
-    self = [super init];
-    if (self) {
-        self.ss = @"ha";
-    }
-=======
 @end
+
 @implementation CTUserModel
 
 - (instancetype)initWithId: (NSString *)uid firstName: (NSString *)fn lastName: (NSString *)ln andLocation: (NSString *)location
@@ -36,7 +30,6 @@
         _location = location;
     }
     return self;
->>>>>>> de298d97b7784d15ae9e05f9c2224ce874d8fe6c
 }
 
 @end
